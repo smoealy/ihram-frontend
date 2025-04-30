0780c392eba1a14852fc979ffa74519576fda569
smoealy <129239220+smoealy@users.noreply.github.com>
2025-04-30 04:50:15 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import AiPlanner from "./pages/AiPlanner.jsx"; // Ensure this file exists
+import App from "./pages/App.jsx";
+import AiPlanner from "./pages/AiPlanner.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
